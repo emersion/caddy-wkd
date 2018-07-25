4ac154eff79883e0dd26bdf9f92b676e3ef4b9b2
emersion <contact@emersion.fr>
2018-07-25 23:41:12 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # caddy-wkd
-OpenPGP Web Key Directory plugin for Caddy
+
+[OpenPGP Web Key Directory][1] plugin for Caddy
+
+## Usage
+
+Put this directive in a HTTP server block:
+
+```
+wks <path to PGP public key>…
+```
+
+## License
+
+MIT
+
+[1]: https://tools.ietf.org/html/draft-koch-openpgp-webkey-service-06
---
+++ caddywkd.go
@@ -0,0 +1,82 @@
+package caddywkd
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/emersion/go-openpgp-wkd"
+	"github.com/mholt/caddy"
+	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"golang.org/x/crypto/openpgp"
+)
+
+func init() {
+	caddy.RegisterPlugin("wkd", caddy.Plugin{
+		ServerType: "http",
+		Action:     setup,
+	})
+}
+
+type plugin struct {
+	Next    httpserver.Handler
+	Pubkeys map[string]openpgp.EntityList
+	Handler wkd.Handler
+}
+
+func (p *plugin) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
+	if !httpserver.Path(r.URL.Path).Matches(wkd.Base) {
+		return p.Next.ServeHTTP(w, r)
+	}
+
+	p.Handler.ServeHTTP(w, r)
+	return 0, nil
+}
+
+func (p *plugin) Discover(hash string) ([]*openpgp.Entity, error) {
+	pubkey, ok := p.Pubkeys[hash]
+	if !ok {
+		return nil, wkd.ErrNotFound
+	}
+	return pubkey, nil
+}
+
+func setup(c *caddy.Controller) error {
+	pubkeys := map[string]openpgp.EntityList{}
+	for c.Next() {
+		if !c.NextArg() {
+			return c.ArgErr()
+		}
+
+		path := c.Val()
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+
+		el, err := openpgp.ReadKeyRing(f)
+		f.Close()
+		if err != nil {
+			return err
+		}
+
+		for _, e := range el {
+			for _, ident := range e.Identities {
+				// TODO: check domain part of the address
+				hash, err := wkd.HashAddress(ident.UserId.Email)
+				if err != nil {
+					return err
+				}
+
+				pubkeys[hash] = append(pubkeys[hash], e)
+			}
+		}
+	}
+
+	httpserver.GetConfig(c).AddMiddleware(func(next httpserver.Handler) httpserver.Handler {
+		p := &plugin{Pubkeys: pubkeys, Next: next}
+		p.Handler.Discover = p.Discover
+		return p
+	})
+
+	return nil
+}
